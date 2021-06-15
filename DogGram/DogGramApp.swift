64c8e293a54ae56cb07b214c4e3b35222ce31dfb
yunus <y.olgun@gmail.com>
2021-06-15 20:53:51 +0300

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by yunus olgun on 15.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
