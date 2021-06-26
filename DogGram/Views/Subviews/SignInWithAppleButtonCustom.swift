ed4694a9df8159f6b5f1925ced849f0b7121201b
yunus <y.olgun@gmail.com>
2021-06-26 12:48:54 +0300

    bolum2-okk

---
+++ DogGram/Views/Subviews/SignInWithAppleButtonCustom.swift
@@ -0,0 +1,21 @@
+//
+//  SignInWithAppleButtonCustom.swift
+//  DogGram
+//
+//  Created by yunus olgun on 20.06.2021.
+//
+
+import Foundation
+import SwiftUI
+import AuthenticationServices
+
+struct SignInWithAppleButtonCustom: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
+        return ASAuthorizationAppleIDButton(authorizationButtonType: .default, authorizationButtonStyle: .black)
+    }
+    
+    func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: Context) {
+        
+    }
+}
